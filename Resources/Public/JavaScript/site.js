ec06b7ad3eb70f1f4b468056b1a21c74033fab10
Thomas Kieslich <post@thomaskieslich.de>
2017-05-30 22:30:05 +0200

    update colorbox

--- Resources/Public/JavaScript/site.js
+++ Resources/Public/JavaScript/site.js
@@ -28,104 +28,114 @@ $('.is-accordion-submenu-parent > a').click(function(){
   });
 
 $(function () {
-    initColorbox();
+	"use strict";
+	initColorbox();
 
-    /**
-     * Smooth scroll
-     */
-    $('.sections-menu a[href*="#"]:not([href="#"]), a[href="#top"]').click(function () {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 400, function () {
-                // $('#offCanvasRight').foundation('close');
-            });
-            return false;
-        }
-    });
+	/**
+	 * Smooth scroll
+	 */
+	$('.sections-menu a[href*="#"]:not([href="#"]), a[href="#top"]').click(function () {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		if (target.length) {
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 400, function () {
+				// $('#offCanvasRight').foundation('close');
+			});
+			return false;
+		}
+	});
 
-    /**
-     * show scroll to top
-     */
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.scrollToTop').fadeIn();
-            $('.scroll-menu').fadeIn();
-        } else {
-            $('.scrollToTop').fadeOut();
-            $('.scroll-menu').fadeOut();
-        }
-    });
+	/**
+	 * show scroll to top
+	 */
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+			$('.scroll-menu').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+			$('.scroll-menu').fadeOut();
+		}
+	});
 
 });
 
 function initColorbox() {
-    $('.lightbox').each(function () {
-        // var lightbox = $('img', this).data('lightbox');
-        // var image = $('img', this).data('1920');
-        // if(lightbox){
-        //     $(this).attr('href', lightbox);
-        // } else{
-        //     $(this).attr('href', image);
-        // }
+	"use strict";
+	$('.lightbox').each(function () {
 
-        $(this).colorbox({
-            rel: $(this).data('lightbox'),
-            title: $('img', this).attr('title'),
-            transition: "fade", //"elastic", "fade", or "none"
-            opacity: 0.9,
-            // width: '80%',
-            // innerWidth: '90%',
-            maxWidth: '90% !important',
-            // height: '90%',
-            // innerHeight: '90%',
-            maxHeight: '90% !important',
-            scalePhotos: true,
-            retinaImage: true,
-            loop: false,
-            slideshow: true,
-            slideshowAuto: true,
-            slideshowSpeed: 2500,
-            slideshowStart: "start slideshow",
-            slideshowStop: "stop slideshow",
-            current: "<span data-current='{current}' data-total='{total}' />",
-            onComplete: function () {
-                lbLoaded();
-                initTouchSwipe();
-            }
-        });
-    });
+		$(this).colorbox({
+			rel: $(this).data('lightbox'),
+			title: $('img', this).attr('title'),
+			transition: "fade", //"elastic", "fade", or "none"
+			fixed: false,
+			opacity: 0.9,
+			// width: '80%',
+			// innerWidth: '90%',
+			maxWidth: '90% !important',
+			// height: '90%',
+			// innerHeight: '90%',
+			maxHeight: '90% !important',
+			scalePhotos: true,
+			retinaImage: true,
+			loop: false,
+			slideshow: true,
+			slideshowAuto: false,
+			slideshowSpeed: 4000,
+			slideshowStart: "start slideshow",
+			slideshowStop: "stop slideshow",
+			current: "<span data-current='{current}' data-total='{total}' />",
+			onOpen : function () {
+				$('html').css({
+					overflow: 'hidden',
+					position: 'fixed'
+				});
+			},
+			onClosed : function () {
+				$('html').css({
+					overflow: 'auto',
+					position: 'static'
+				});
+			},
+			onComplete: function () {
+				lbLoaded();
+				initTouchSwipe();
+			}
+		});
+	});
+
 }
 
 function lbLoaded() {
-    var total = $('#colorbox #cboxCurrent span').data('total');
-    var current = $('#colorbox #cboxCurrent span').data('current');
-    var images = {
-        'de': 'Bild ' + current + ' von ' + total,
-        'en': 'image ' + current + ' from ' + total
-    };
-    $('#colorbox #cboxCurrent').text(images[$('html').attr('lang')]);
-    //hide Play on end
-    if (current == total) {
-        $('#colorbox #cboxSlideshow').hide();
-    } else {
-        $('#colorbox #cboxSlideshow').show();
-    }
+	"use strict";
+	var total = $('#colorbox #cboxCurrent span').data('total');
+	var current = $('#colorbox #cboxCurrent span').data('current');
+	var images = {
+		'de': 'Bild ' + current + ' von ' + total,
+		'en': 'image ' + current + ' from ' + total
+	};
+	$('#colorbox #cboxCurrent').text(images[$('html').attr('lang')]);
+	//hide Play on end
+	if (current === total) {
+		$('#colorbox #cboxSlideshow').hide();
+	} else {
+		$('#colorbox #cboxSlideshow').show();
+	}
 }
 
 function initTouchSwipe() {
-    $('#colorbox').swipe({
-        swipeLeft: function (event, direction, distance, duration, fingerCount) {
-            $.colorbox.prev();
-        },
-        swipeRight: function (event, direction, distance, duration, fingerCount) {
-            $.colorbox.next();
-        },
-        //Default is 75px, set to 0 for demo so any distance triggers swipe
-        threshold: 75
-    });
+	"use strict";
+	$('#colorbox').swipe({
+		swipeLeft: function () {
+			$.colorbox.prev();
+		},
+		swipeRight: function () {
+			$.colorbox.next();
+		},
+		threshold: 75
+	});
 }
 
 //# sourceMappingURL=maps/site.js.map
