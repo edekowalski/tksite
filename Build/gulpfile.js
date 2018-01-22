b43454ab1ff6d0e6eb0f2442839943ad92e78329
Thomas Kieslich <post@thomaskieslich.de>
2018-01-22 23:08:01 +0100

    start with gulp 4.0

--- Build/gulpfile.js
+++ Build/gulpfile.js
@@ -1,104 +1,83 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	scsslint = require('gulp-scss-lint'),
-	sourcemaps = require('gulp-sourcemaps'),
-	autoprefixer = require('gulp-autoprefixer'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	cleanCSS = require('gulp-clean-css'),
-	rename = require('gulp-rename'),
-	svgmin = require('gulp-svgmin'),
-	svg2png = require('gulp-svg2png'),
-	sequence = require('run-sequence')
-;
+'use strict';
 
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins');
+const rimraf = require('rimraf');
 
-// File paths to various assets are defined here.
+const $ = plugins();
+const PRODUCTION = false;
+
 var PATHS = {
-	site_src: '../Resources/Private/',
-	site_dist: '../Resources/Public/',
-	js_libs: [
-		//jquery
-        'bower_components/jquery/dist/jquery.min.js',
-
-        //Foundation
-        // 'bower_components/what-input/what-input.min.js',
-        'bower_components/foundation-sites/dist/js/foundation.min.js',
-
-		//other
-		// 'bower_components/bx-slider.js/dist/jquery.bxslider.min.js',
-		'bower_components/colorbox/jquery.colorbox-min.js',
-		'bower_components/jquery-touchSwipe/jquery.touchSwipe.min.js'
-
+	assets: 'assets/**/*',
+	js: [
+		'node_modules/jquery/dist/jquery.min.js',
+		"node_modules/foundation-sites/dist/js/foundation.min.js",
+		"node_modules/jquery-colorbox/jquery.colorbox-min.js",
+		"node_modules/jquery-touchswipe/jquery.touchswipe.min.js",
+		"node_modules/prismjs/prism.js",
+		"js/components/**/*.js",
+		"js/*.js"
 	],
-	site_js: [
-		//foundation
-		'../Resources/Private/JavaScript/foundation.js',
-
-		//jqueryui
-		// '../Resources/Private/JavaScript/jqueryui.js',
-
-		//site
-		// '../Resources/Private/JavaScript/viewport.js',
-		// '../Resources/Private/JavaScript/storage.js',
-
-		'../Resources/Private/JavaScript/base.js'
-	]
+	dist: '../Resources/Public/'
 };
 
-gulp.task('jsLibs', function () {
-	return gulp.src(PATHS.js_libs)
-		.pipe(concat('site-libs.min.js'))
-		// .pipe(uglify())
-		.pipe(gulp.dest(PATHS.site_dist + 'JavaScript/'));
-});
+gulp.task('build',
+	gulp.series(
+		clean,
+		gulp.parallel(sass, javascript, svg),
+		copy
+	)
+);
 
-gulp.task('sass', function () {
-	return gulp.src(PATHS.site_src + 'Styles/*.scss')
-		.pipe(sourcemaps.init())
-		.pipe(scsslint({'config': 'scss-lint.yml'}))
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer(["last 2 versions"], {cascade: true}))
-		.pipe(sourcemaps.write('./maps'))
-		.pipe(gulp.dest(PATHS.site_dist + 'Styles/'));
-});
+gulp.task('default',
+	gulp.series('build', watch)
+);
 
-gulp.task('javascript', function () {
-	return gulp.src(PATHS.site_js)
-		.pipe(sourcemaps.init())
-		.pipe(concat('site.js'))
-		.pipe(uglify())
-		.pipe(sourcemaps.write('./maps'))
-		.pipe(gulp.dest(PATHS.site_dist + 'JavaScript/'));
-});
+function clean(done) {
+	rimraf(PATHS.dist + '*', done);
+}
 
-gulp.task('minify', function () {
-	gulp.src([PATHS.site_dist + 'JavaScript/*.js', '!' + PATHS.site_dist + 'JavaScript/*.min.js'])
-		.pipe(uglify())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest(PATHS.site_dist + 'JavaScript/'));
-	return gulp.src([PATHS.site_dist + 'Styles/*.css', '!' + PATHS.site_dist + 'Styles/*.min.css'])
-		.pipe(cleanCSS())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest(PATHS.site_dist + 'Styles/'));
-});
+function copy() {
+	return gulp.src(PATHS.assets)
+		.pipe(gulp.dest(PATHS.dist));
+}
 
-gulp.task('icons', function () {
-	gulp.src('assets/page/*.svg')
-		.pipe(svgmin())
-		.pipe(gulp.dest(PATHS.site_dist + 'Icons/Page'));
-	return gulp.src('assets/site/*.svg')
-		.pipe(svgmin())
-		.pipe(gulp.dest(PATHS.site_dist + 'Icons/'))
-		.pipe(svg2png())
-		.pipe(gulp.dest(PATHS.site_dist + 'Icons/'));
-});
+function sass() {
+	return gulp.src('scss/*.scss')
+		.pipe($.sourcemaps.init())
+		.pipe($.sass({
+			includePaths: PATHS.sass
+		})
+			.on('error', $.sass.logError))
+		.pipe($.autoprefixer({
+			browsers: [
+				'last 2 versions',
+				'ie >= 11',
+				'ios >= 9'
+			]
+		}))
+		.pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+		.pipe(gulp.dest(PATHS.dist + 'Styles'));
+}
 
-gulp.task('build', function (done) {
-	sequence(['sass', 'jsLibs', 'javascript', 'icons', 'minify'], done);
-});
+function javascript() {
+	return gulp.src(PATHS.js)
+		.pipe($.sourcemaps.init())
+		.pipe($.if(!PRODUCTION, $.uglify()))
+		.pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+		.pipe($.concat('app.js'))
+		.pipe(gulp.dest(PATHS.dist + 'JavaScript'));
+}
 
-gulp.task('default', function () {
-	gulp.watch(PATHS.site_src + 'Styles/**/*.scss', ['sass', 'minify']);
-	gulp.watch(PATHS.site_src + 'JavaScript/**/*.js', ['javascript', 'minify']);
-});
+function svg() {
+  return gulp.src('svg/**/*.svg')
+    .pipe($.svgmin())
+    .pipe(gulp.dest(PATHS.dist + 'Icons'));
+}
+
+function watch() {
+  gulp.watch('assets/**/*', copy);
+  gulp.watch('scss/**/*.scss', sass);
+  gulp.watch('js/**/*.js', javascript);
+  gulp.watch('svg/**/*.svg', svg);
+}
